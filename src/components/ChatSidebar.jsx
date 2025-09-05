e79e23ef397a3d33d11b46c1c3fbd2ae30678768
Shahid Raza <engineerbro0098@gmail.com>
2025-09-05 23:06:31 +0530

    add flowbite button

--- src/components/ChatSidebar.jsx
+++ src/components/ChatSidebar.jsx
@@ -90,11 +90,12 @@ export default function ChatSidebar({ editor, onAiEdit }) {
           onKeyDown={(e) => e.key === "Enter" && sendMessage()}
         />
         <button
-          className="bg-blue-500 text-white px-3 py-1 rounded disabled:opacity-50"
+          className="relative cursor-pointer inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
           onClick={sendMessage}
           disabled={loading}
         >
-          Send
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-[#725CAD] dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">Send</span>
+          
         </button>
       </div>
     </div>
